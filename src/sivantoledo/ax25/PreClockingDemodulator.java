23cb5f84897049d92ee79c44931cfd4290196ba0
robbieford <robbieford2000@msn.com>
2014-06-01 17:32:35 -0700

    adding the Preclocking demodulator

--- src/sivantoledo/ax25/PreClockingDemodulator.java
+++ src/sivantoledo/ax25/PreClockingDemodulator.java
@@ -19,18 +19,26 @@
  */
 package sivantoledo.ax25;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+
+import com.sun.org.apache.xalan.internal.utils.FeatureManager.Feature;
 
 public class PreClockingDemodulator
         extends PacketDemodulator //implements HalfduplexSoundcardClient 
 {
 
-    private float[] td_filter;
+    public enum Frequency {
+    	f_1200,
+    	f_2200
+	}
+    
+	private float[] td_filter;
     private float[] cd_filter;
     private int rate_index;
-    private int sample_rate;
+    private int sampleRate;
     //private int samples_per_bit;
-    private float samples_per_bit;
+    private float samplesPerBaud;
     //private float[] u1, u2, x, f0_cos, f0_sin, f1_cos, f1_sin;
     private float[] u1, x;
     private float[] c0_real, c0_imag, c1_real, c1_imag;
@@ -38,7 +46,7 @@ public class PreClockingDemodulator
     //private float[] fdiff;
     private float previous_fdiff;
     private int f0_i = 0, f1_i = 0;
-    private int last_transition;
+    private long last_transition;
     private int data, bitcount;
     private int vox_countdown = 0;
     private float vox_threshold = 0.1f;
@@ -46,6 +54,7 @@ public class PreClockingDemodulator
     private float phase_inc_symbol;
     private Packet packet; // received packet
     private PacketHandler handler;
+    private int samplesInFlag;
     
     private static enum State {
 
@@ -89,6 +98,8 @@ public class PreClockingDemodulator
 
     public PreClockingDemodulator(int sample_rate, int filter_length, int emphasis, PacketHandler h) throws Exception {
         super(sample_rate == 8000 ? 16000 : sample_rate);
+        
+        filteredSampleArray = new ArrayList<Float>(); //Needs to big enough for the filter
 
         if (sample_rate == 8000) {
             interpolate = true;
@@ -107,9 +118,10 @@ public class PreClockingDemodulator
         }
 
         handler = h;
-        this.sample_rate = sample_rate;
-        this.samples_per_bit = (float) sample_rate / 1200.0f;
-        System.err.printf("samples per bit = %.3f\n", this.samples_per_bit);
+        this.sampleRate = sample_rate;
+        this.samplesPerBaud = (float) sample_rate / 1200.0f;
+        samplesInFlag = Math.round(samplesPerBaud * FLAG_LENGTH_IN_BAUD);
+        System.err.printf("samples per bit = %.3f\n", this.samplesPerBaud);
         //this.samples_per_bit = Afsk1200Filters.bit_periods[rate_index]; // this needs to be computed locally
 
         //if (samples_per_bit * 1200 != sample_rate) {
@@ -163,10 +175,10 @@ public class PreClockingDemodulator
         //f1_cos = new float[6*samples_per_bit]; // f0=2200Hz so 11 cycles fit exactly in 6 bit periods.
         //f1_sin = new float[6*samples_per_bit]; // otherwise we would have had to compute sin/cos
 
-        c0_real = new float[(int) Math.floor(samples_per_bit)];
-        c0_imag = new float[(int) Math.floor(samples_per_bit)];
-        c1_real = new float[(int) Math.floor(samples_per_bit)];
-        c1_imag = new float[(int) Math.floor(samples_per_bit)];
+        c0_real = new float[(int) Math.floor(samplesPerBaud)];
+        c0_imag = new float[(int) Math.floor(samplesPerBaud)];
+        c1_real = new float[(int) Math.floor(samplesPerBaud)];
+        c1_imag = new float[(int) Math.floor(samplesPerBaud)];
 
         //diff    = new float[samples_per_bit];
         //fdiff   = new float[samples_per_bit];
@@ -188,6 +200,11 @@ public class PreClockingDemodulator
         //}
 
         //System.out.printf("Size of symbol sync filter is %d\n", symbol_sync_filter.length);
+
+        sampleArray = new ArrayList<Float>(td_filter.length);
+        for (int i= 0; i < td_filter.length; i++) {
+        	sampleArray.add(0.0f);
+        }
     }
     private volatile boolean data_carrier = false;
 
@@ -224,7 +241,7 @@ public class PreClockingDemodulator
     private float phase_f0, phase_f1;
     //private int j;    // sample index, rolls over each bit period 
     //private int j_f1; // sample index
-    private int t; // running sample counter
+    private long t; // running sample counter
     private float f1cos, f1sin, f0cos, f0sin;
     //public void addSamples(float[] s) {
     //	addSamples(s,s.length);
@@ -244,10 +261,18 @@ public class PreClockingDemodulator
      * 	d.) Use clocking to do correlation. (per baud)
      */
     
-    private float sampleArray[];
-    private float filteredSampleArray[];
+	/**
+	 * Add sample to filter buffer and filter the sample
+	 * Start looking for flags...
+	 *	If flag found store the location and look at distance since last flag
+	 * If flags 
+	 */
+	
+    private ArrayList<Float> sampleArray; //Needs to big enough for the filter
+    private ArrayList<Float> filteredSampleArray; //Slightly bigger than max packet
     private static final int MINIMUM_PACKET_BYTES = 17;
     private static final int MAXIMUM_PACKET_BYTES = 256; //MTU for AX25
+	private static final int FLAG_LENGTH_IN_BAUD = 9; //0x7E
     
     protected void addSamplesPrivate(float[] s, int n) {
         int i = 0;
@@ -272,6 +297,270 @@ public class PreClockingDemodulator
         }
     }
     
+    private boolean containsStartFlag = false;
+    
+    private static final int BUFFER_SAMPLES_AFTER_FLAG = 10;
+    
+	protected void addSamplePrivate(float sample) {
+		//Add sample to filter input array
+		sampleArray.add(0, sample);
+		sampleArray.remove(sampleArray.size() -1);
+
+		//Filter
+		float filteredSample = Filter.filter(sampleArray, td_filter);
+		//sample store filtered sample
+		/**
+		 * _______________________________________________________
+		 * WE ARENT FILTERING ANYTHING!!!!!!! 
+		 * _______________________________________________________
+		 */
+		filteredSampleArray.add(sample);
+		//filteredSampleArray.add(filteredSample);
+		
+		//Look for flags in samples
+		boolean flagInSamples = containsFlag(filteredSample);
+		if (flagInSamples) {
+			//something
+			//System.out.println("Saw a flag! " + t);
+			if (containsStartFlag) {
+				int bytesBetweenFlags =(int) ((filteredSampleArray.size() - samplesInFlag*2 - BUFFER_SAMPLES_AFTER_FLAG)/samplesPerBaud)/8;
+				
+				if(bytesBetweenFlags >= MINIMUM_PACKET_BYTES && bytesBetweenFlags <= MAXIMUM_PACKET_BYTES ) {
+					System.out.println("Number of Potential Packtes: " + ++numPotentialPackets + " with size: " + bytesBetweenFlags);
+					processPacket();
+					shrinkFilteredSampleArray();
+				} else {
+					shrinkFilteredSampleArray();
+				}
+				
+			} else {
+				containsStartFlag = true;
+				shrinkFilteredSampleArray();
+			}
+		}
+		//Look in area since previous flag
+		//Flag handling logic
+		ArrayList<Float> packetSamples;
+		//processPacket(packetSamples);
+	}
+	
+	int numPotentialPackets = 0;
+	
+	private void processPacket() {
+		ArrayList<Float> derivativeOfSamples = getDerivativeOfFilteredData();
+		ArrayList<Float> derivativeCrossingPositions = calulateZeroCrossings(derivativeOfSamples);
+		ArrayList<Float> frequenciesFromDerivative = getFrequenciesSampleList(derivativeCrossingPositions);
+		ArrayList<Float> detectedFreqTransitions = getTransitionsFromFreqData(frequenciesFromDerivative);
+		int combOffset = getCombIndex(detectedFreqTransitions);
+		ArrayList<Frequency> frequencySymbolList = getFrequencySymbolList(combOffset, frequenciesFromDerivative, derivativeCrossingPositions);
+		generatePacket(frequencySymbolList);
+	}
+	
+	private void generatePacket(ArrayList<Frequency> frequencySymbolList) {
+		Frequency lastFreq = frequencySymbolList.get(0);
+		int bauds = 0;
+		
+		for (int i = 0; i < frequencySymbolList.size(); i++) {
+			bauds++;
+			if (frequencySymbolList.get(i) != lastFreq) {
+				handleDemodulatedBits(bauds);
+				bauds = 0;
+				lastFreq = frequencySymbolList.get(i);
+			}
+		}
+	}
+
+	private ArrayList<Frequency> getFrequencySymbolList(int combOffset, ArrayList<Float> frequenciesFromDerivative, ArrayList<Float> derivativeCrossingPositions) {
+		ArrayList<Frequency> frequencies = new ArrayList<Frequency>();
+		
+		//Handle First
+		//frequencies.add(getFrequency(0, combOffset, frequenciesFromDerivative, derivativeCrossingPositions));
+		
+		for (Float i = new Float(combOffset); i < filteredSampleArray.size(); i+=samplesPerBaud) {
+			Frequency freq = getFrequency(i + 1, i+samplesPerBaud, frequenciesFromDerivative, derivativeCrossingPositions);
+			if (freq != null){
+				frequencies.add(freq);
+			}
+		}
+		
+		return frequencies;
+	}
+
+	private Frequency getFrequency(float start, float end, ArrayList<Float> frequenciesFromDerivative,ArrayList<Float> derivativeCrossingPositions) {
+		int startIdx=-1, endIdx=-1;
+		float sum = 0.0f;
+		
+		for (int i = 0; i < derivativeCrossingPositions.size(); i ++) {
+			if (startIdx < 0 && derivativeCrossingPositions.get(i) > start) {
+				startIdx = i;
+			}
+			if (endIdx < 0 && derivativeCrossingPositions.get(i) > end) {
+				endIdx = i - 1;
+			}
+		}
+		
+		if (endIdx == -1 || startIdx == -1) {
+			return null;
+		}
+		
+		for (int i = startIdx; i <= endIdx; i++) {
+			sum += frequenciesFromDerivative.get(i);
+		}
+		
+		//Frequency freq = sum / (endIdx + 1 - startIdx) > 1850 ? Frequency.f_2200: Frequency.f_1200;
+		Frequency freq =  frequenciesFromDerivative.get((endIdx + startIdx) /2) > 1850 ? Frequency.f_2200: Frequency.f_1200;
+		//System.out.println(freq + " actual value: " + sum / (endIdx + 1 - startIdx) + " num of freqs in avg: " + (endIdx + 1 - startIdx));
+		return freq;
+	}
+
+	private int getCombIndex(ArrayList<Float> detectedFreqTransitions) {
+		long minimumSquaredDistance = -1;
+		int sampleCombPosition = 0;
+		
+		ArrayList<Integer> modulusValues = new ArrayList<Integer>(detectedFreqTransitions.size());
+		
+		for (int i = 0; i < detectedFreqTransitions.size(); i ++) {
+			modulusValues.add(Math.round(detectedFreqTransitions.get(i)) % Math.round(samplesPerBaud));
+		}
+		
+		for (int i = 0; i < samplesPerBaud; i ++) {
+			long squaredDistance = 0;
+			for (int j = 0; j < detectedFreqTransitions.size(); j++){
+				squaredDistance += Math.pow(modulusValues.get(j) - i, 2);
+			}
+			if (minimumSquaredDistance == -1 || minimumSquaredDistance > squaredDistance) {
+				minimumSquaredDistance = squaredDistance;
+				sampleCombPosition = i;
+			}
+		}
+		
+		return sampleCombPosition;
+	}
+
+	private ArrayList<Float> getTransitionsFromFreqData(ArrayList<Float> freqs) {
+		ArrayList<Float> sampleNumbers = new ArrayList<Float>();
+		
+		for (int i = 1; i < freqs.size(); i ++){
+			if (isZeroCrossing(freqs.get(i - 1) - 1700.0f, freqs.get(i) - 1700.0f)) {
+				sampleNumbers.add(i - 1 + zeroCrossingPercentage(freqs.get(i-1) - 1700.0f, freqs.get(i) - 1700.0f));
+			}
+		}
+		
+		return sampleNumbers;
+	}
+
+	private ArrayList<Float> getFrequenciesSampleList(ArrayList<Float> data) {
+		ArrayList<Float> freqs = new ArrayList<Float>();
+		
+		for (int i = 1; i < data.size(); i++){
+			freqs.add(sampleRate / (data.get(i) - data.get(i-1)) / 2.0f);
+		}
+		
+		return freqs;
+	}
+
+	private ArrayList<Float> calulateZeroCrossings(ArrayList<Float> samples) {
+		ArrayList<Float> crossingList = new ArrayList<Float>();
+		
+		for (int i = 1; i < samples.size(); i++) {
+			if (isZeroCrossing(samples.get(i-1), samples.get(i))){
+				crossingList.add(i - 1 + zeroCrossingPercentage(samples.get(i-1), samples.get(i)));
+			}
+		}
+		
+		return crossingList;
+	}
+	
+    protected boolean isZeroCrossing(float firstSample, float secondSample) {
+    	return ((firstSample <= 0 && secondSample > 0) || //Going from below zero to above OR
+		(firstSample >= 0 && secondSample < 0)) ;			// Going from above zer to below
+    }
+    
+    protected float zeroCrossingPercentage(float firstSample, float secondSample) {
+    	//x = x1 + (x2  - x1) * ((0-y1)/(y2-y1))
+    	return (-firstSample /(secondSample - firstSample));
+    }
+
+	private ArrayList<Float> getDerivativeOfFilteredData() {
+		ArrayList<Float> derivative = new ArrayList<Float>(filteredSampleArray.size());
+		
+		for (int i = 1; i < filteredSampleArray.size() - 1; i++){
+			derivative.add(filteredSampleArray.get(i+1) - filteredSampleArray.get(i-1));
+			if (derivative.size() == 1){
+				derivative.add(filteredSampleArray.get(i+1) - filteredSampleArray.get(i-1));
+			}
+		}
+		derivative.add(derivative.get(derivative.size() - 1));
+		
+		return derivative;
+	}
+	
+	private void shrinkFilteredSampleArray() {
+		filteredSampleArray = new ArrayList<Float> (filteredSampleArray.subList(filteredSampleArray.size() - 1 - samplesInFlag - BUFFER_SAMPLES_AFTER_FLAG,
+				filteredSampleArray.size() - 1));
+	}
+	
+	private boolean containsFlag(float sample) {
+		boolean retVal = false;
+
+		c0_real[j_corr] = sample * (float) Math.cos(phase_f0);
+		c0_imag[j_corr] = sample * (float) Math.sin(phase_f0);
+
+		c1_real[j_corr] = sample * (float) Math.cos(phase_f1);
+		c1_imag[j_corr] = sample * (float) Math.sin(phase_f1);
+
+		phase_f0 += phase_inc_f0;
+		if (phase_f0 > (float) 2.0 * Math.PI) {
+			phase_f0 -= (float) 2.0 * Math.PI;
+		}
+		phase_f1 += phase_inc_f1;
+		if (phase_f1 > (float) 2.0 * Math.PI) {
+			phase_f1 -= (float) 2.0 * Math.PI;
+		}
+
+		float cr = sum(c0_real, j_corr);
+		float ci = sum(c0_imag, j_corr);
+		float c0 = (float) Math.sqrt(cr * cr + ci * ci);
+
+		cr = sum(c1_real, j_corr);
+		ci = sum(c1_imag, j_corr);
+		float c1 = (float) Math.sqrt(cr * cr + ci * ci);
+
+		diff[j_cd] = c0 - c1;
+		float fdiff = Filter.filter(diff, j_cd, cd_filter);
+
+		if (previous_fdiff * fdiff < 0 || previous_fdiff == 0) {
+
+			// we found a transition
+			int p = (int)(t - last_transition);
+			last_transition = t;
+
+			int bits = (int) Math.round((double) p / (double) samplesPerBaud);
+			//System.out.printf("$ %f %d\n",(double) p / (double)samples_per_bit,bits);
+
+			if (bits == 7) {
+				retVal = true;
+			}
+		}
+
+		previous_fdiff = fdiff;
+
+		t++;
+
+		j_cd++;
+		if (j_cd == cd_filter.length) {
+			j_cd = 0;
+		}
+
+		j_corr++;
+		if (j_corr == c0_real.length ) {// samples_per_bit
+			j_corr = 0;
+		}
+		
+		return retVal;
+	}
+	
+	/**
     protected void addSamplePrivate(float sample) {
         u1[j_td] = sample;
         x[j_td] = Filter.filter(u1, j_td, td_filter);
@@ -354,12 +643,14 @@ public class PreClockingDemodulator
         }
 
         j_corr++;
-        if (j_corr == c0_real.length /* samples_per_bit*/) {
+        if (j_corr == c0_real.length ) {// samples_per_bit
             j_corr = 0;
         }
     }
+	*/
     
     private void handleDemodulatedBits(int bits) {
+    	//System.out.println(bits);
         if (bits == 0 || bits > 7) {
             state = State.WAITING;
             data_carrier = false;
