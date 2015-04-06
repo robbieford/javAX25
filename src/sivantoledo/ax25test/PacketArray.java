148861febc9b616b98e71857a87e97248faaef4c
Robbie Campbell <robbieford2000@msn.com>
2015-04-06 11:47:35 -0700

    Update to use array.equals instead of a home brewed solution

--- src/sivantoledo/ax25test/PacketArray.java
+++ src/sivantoledo/ax25test/PacketArray.java
@@ -1,20 +1,26 @@
 package sivantoledo.ax25test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PacketArray extends ArrayList<byte[]> {
 
+	public int packet_count = 0;
+	public int dupCount = 0;
+	
 	/**
 	 * Randomly Generated
 	 */
 	private static final long serialVersionUID = -5932719560958109010L;
+	private ArrayList<byte[]> lastPackets = new ArrayList<byte[]>();
 	
 	@Override
 	public boolean contains(Object other) {
 		if (other instanceof byte[]){
 			byte[] otherPacket = (byte[]) other;
 			for (byte[] packet : this) {
-				if (areSamePacket(packet, otherPacket)){
+				if (Arrays.equals(packet, otherPacket))
+				{
 					return true;
 				}
 			}
@@ -25,17 +31,15 @@ public class PacketArray extends ArrayList<byte[]> {
 		return false;
 	}
 
-	private boolean areSamePacket(byte[] a, byte[] b) {
-		if (a.length == b.length) {
-			for (int i = 0; i < a.length; i++) {
-				if (a[i] != b[i]) {
-					return false;
-				}
-			}
-		} else {
+	public boolean addWithReturn(byte[] packet) {
+
+		if (this.contains(packet) ) {
+			dupCount++;
 			return false;
+		} else {
+			packet_count++;
+			this.add(packet);
+			return true;
 		}
-		
-		return true;
 	}
 }
