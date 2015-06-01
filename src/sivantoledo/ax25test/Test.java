d79baede96f763b484bff7a1d92c775277a2deec
Robbie Campbell <robbieford2000@msn.com>
2015-06-01 00:01:27 -0700

    Add Derivative demodulator to the test file

--- src/sivantoledo/ax25test/Test.java
+++ src/sivantoledo/ax25test/Test.java
@@ -41,6 +41,7 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 import sivantoledo.ax25.Afsk1200Modulator;
 import sivantoledo.ax25.Afsk1200MultiDemodulator;
 import sivantoledo.ax25.Arrays;
+import sivantoledo.ax25.DerivativeZeroCrossingDemodulator;
 import sivantoledo.ax25.GoertzelDemodulator;
 import sivantoledo.ax25.GoertzelMaxClockingDemodulator;
 import sivantoledo.ax25.GoertzelPreClockingDemodulator;
@@ -192,20 +193,21 @@ public class Test implements PacketHandler {
 		PacketDemodulator multi = null;
 		try {
 //			afsk = new Afsk1200(rate,filter_length,0,t);
-//			multi = new Afsk1200Demodulator(rate,filter_length,1,t);
+//			multi = new Afsk1200Demodulator(rate,filter_length,6,t);
 //			multi = new Afsk1200MultiDemodulatorPlus(rate,t);
 //
 //			multi = new ZeroCrossingDemodulator(rate,filter_length,3,t);
 //			multi = new StrictZeroCrossingDemodulator(rate,filter_length,3,t);
-//			multi = new PeakDemodulator(rate,filter_length,0,t);
-			multi = new GoertzelDemodulator(rate, filter_length, -1, t, 1);
+//			multi = new PeakDemodulator(rate,filter_length,1,t);
+//			multi = new GoertzelDemodulator(rate, filter_length, -1, t, 1);
 //			multi = new ZeroCrossingDemodulator(rate,filter_length,1,t);
 //			multi = new StrictZeroCrossingDemodulator(rate,filter_length,6,t);
+//			multi = new DerivativeZeroCrossingDemodulator(rate,filter_length,1,t);
 //			multi = new MixedPreClockingDemodulator(rate, filter_length,0,t);
 //			multi = new GoertzelPreClockingDemodulator(rate, filter_length,1,t);
-//			multi = new GoertzelMaxClockingDemodulator(rate, filter_length,1,t);
+			multi = new GoertzelMaxClockingDemodulator(rate, filter_length,1,t);
 //			multi = new ThreadedGoertzelMaxClockingDemodulator(rate, filter_length,1,t);
-//			multi = new WindowedZeroCrossingDemodulator(rate, filter_length, 1, t);
+//			multi = new WindowedZeroCrossingDemodulator(rate, filter_length, 3, t);
 //			multi = new PLLDemodulator(rate, filter_length, 1, t);
 			
 			mod = new Afsk1200Modulator(rate);
